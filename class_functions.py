0fa4f78571d8a4b99a124f34f17775854072c6a2
Shaun <98676425+shaunlwl@users.noreply.github.com>
2022-11-16 10:42:31 +0800

    Created repository

---
+++ class_functions.py
@@ -0,0 +1,23 @@
+
+def LoadInitialData():
+    airlines = []
+    aircraft_type = []
+    flights = []
+
+
+class airlines:
+  def __init__(self, name, age):
+    self._name = name
+    self._age = age
+
+
+class aircraft_type:
+  def __init__(self, name, age):
+    self._name = name
+    self._age = age
+
+
+class flights:
+  def __init__(self, name, age):
+    self._name = name
+    self._age = age
---
+++ pyflights.py
@@ -0,0 +1,11 @@
+import datetime as dt
+import class_functions as cf
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
