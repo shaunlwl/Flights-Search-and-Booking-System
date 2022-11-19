18b87f996befa8deacb737b7ce198e9d8d1e8516
Shaun <98676425+shaunlwl@users.noreply.github.com>
2022-11-19 21:07:39 +0800

    Created required classes and completed use case 1,2,3

--- class_functions.py
+++ class_functions.py
@@ -1,23 +1,217 @@
+import datetime as dt
 
-def LoadInitialData():
-    airlines = []
-    aircraft_type = []
-    flights = []
+def LoadInitialData(airlines_list, aircraft_type_list, scheduled_flights):
+  
+  #Load Airlines
+  SQ = airlines("Singapore Airlines","SQ")
+  QF = airlines("Qantas", "QF")
+  airlines_list.append(SQ)
+  airlines_list.append(QF)
 
+  #Load Aircraft Type
+  aircraft_type_list.append(aircraft_type("Boeing 747", ["F", "J", "Y"], {"F":"1-1-1","J":"2-2-2","Y":"3-4-3"}, {"F":[1,10],"J":[11,20],"Y":[21,50]}))
+  SQ.setAircraftType(aircraft_type_list[0])
+  QF.setAircraftType(aircraft_type_list[0])
+
+  #Load Flights
+  scheduled_flights.append(flights("SQ", "123","SIN", "NRT", dt.datetime.strptime("5 Dec 22, 10:00AM", '%d %b %y, %I:%M%p'), 6.5, 3, "Boeing 747", {"F": 2000, "J": 1000, "Y":500}, "321"))
+  scheduled_flights.append(flights("SQ", "456","SYD", "SIN", dt.datetime.strptime("4 Dec 22, 10:00PM", '%d %b %y, %I:%M%p'), 8, 2, "Boeing 747", {"F": 1500, "J": 750, "Y":300}, "654"))
+  scheduled_flights.append(flights("SQ", "789","SYD", "NRT", dt.datetime.strptime("5 Dec 22, 09:00AM", '%d %b %y, %I:%M%p'), 14, 2, "Boeing 747", {"F": 3500, "J": 2500, "Y":1500}, "987"))
+  scheduled_flights.append(flights("QF", "789","SYD", "NRT", dt.datetime.strptime("5 Dec 22, 01:00PM", '%d %b %y, %I:%M%p'), 14.5, 2, "Boeing 747", {"F": 3200, "J": 2200, "Y":1200}, "987"))
+  scheduled_flights.append(flights("SQ", "123","SIN", "NRT", dt.datetime.strptime("12 Dec 22, 10:00AM", '%d %b %y, %I:%M%p'), 6.5, 3, "Boeing 747", {"F": 2000, "J": 1000, "Y":500}, "321"))
+  scheduled_flights.append(flights("SQ", "456","SYD", "SIN", dt.datetime.strptime("11 Dec 22, 10:00PM", '%d %b %y, %I:%M%p'), 8, 2, "Boeing 747", {"F": 1500, "J": 750, "Y":300}, "654"))
+  scheduled_flights.append(flights("SQ", "789","SYD", "NRT", dt.datetime.strptime("12 Dec 22, 09:00AM", '%d %b %y, %I:%M%p'), 14, 2, "Boeing 747", {"F": 3500, "J": 2500, "Y":1500}, "987"))
+  scheduled_flights.append(flights("QF", "789","SYD", "NRT", dt.datetime.strptime("12 Dec 22, 01:00PM", '%d %b %y, %I:%M%p'), 14.5, 2, "Boeing 747", {"F": 3200, "J": 2200, "Y":1200}, "987"))
+  
+  return airlines_list, aircraft_type_list, scheduled_flights 
+
+
+
+def createAirline(name, IATA_code, airlines_list):
+  airlines_list.append(airlines(name, IATA_code))
+
+
+
+#Airlines Class
 
 class airlines:
-  def __init__(self, name, age):
+  def __init__(self, name, IATA_code):
     self._name = name
-    self._age = age
+    self._IATA = IATA_code
+    self._aircraft_type = []
 
+  def getName(self):
+    return self._name
+
+  def setName(self, name):
+    self._name = name
+    
+
+  def getIATA(self):
+    return self._IATA
+
+  def setIATA(self, IATA_code):
+    self._IATA = IATA_code
+  
+  def getAircraftType(self):
+    return self._aircraft_type
+
+  def setAircraftType(self, aircraft_type):
+    self._aircraft_type.append(aircraft_type)
+    
+
+
+
+#Aircraft Type Class
 
 class aircraft_type:
-  def __init__(self, name, age):
+  def __init__(self, name, cabin_class, seat_config, seat_start_end):
     self._name = name
-    self._age = age
+    self._cabin_class = cabin_class
+    self._seat_config = seat_config
+    self._cabin_start_end_rows = seat_start_end
+    self._seats_layout = {}
+    
+    if len(list(seat_config.keys())) != len(list(seat_start_end.keys())):
+      
+      print("\nERROR: Seat Configuration and Start and End Rows per Cabin Class have errorneous information\n")
+    
+    else:
 
+      for i in range(0,len(list(seat_config.keys()))): #Choose the cabin class
+        
+        
+        seat_arrangement = seat_config[list(seat_config.keys())[i]].split("-")    #[1,1,1]
+        row_number = seat_start_end[list(seat_start_end.keys())[i]][0]
+        
+        while row_number <= seat_start_end[list(seat_start_end.keys())[i]][1]:
+          start_char = "A"
+                    
+          for items in seat_arrangement:
+            items = int(items)
+            j = 1
+
+            while j <= items: 
+              
+              if list(seat_config.keys())[i] not in self._seats_layout.keys():
+                self._seats_layout[list(seat_config.keys())[i]] = [str(row_number)+start_char]
+              else:
+                self._seats_layout[list(seat_config.keys())[i]].append(str(row_number)+start_char)
+              j += 1
+              start_char = chr(ord(start_char) + 1)
+          
+          row_number += 1
+
+      
+
+        
+
+
+  def getName(self):
+    return self._name
+
+  def setName(self, name):
+    self._name = name
+    
+  def getCabinClass(self):
+    return self._cabin_class
+
+  def setCabinClass(self, cabin_class):
+    self._cabin_class = cabin_class
+
+  def getSeatConfig(self):
+    return self._seat_config
+
+  def setSeatConfig(self, seat_config):
+    self._seat_config = seat_config
+  
+  def getCabinStartandEnd(self):
+    return self._cabin_start_end_rows
+
+  def setCabinStartandEnd(self, seat_start_end):
+    self._cabin_start_end_rows = seat_start_end
+
+  def getSeatsLayout(self):
+    return self._seats_layout
+
+
+
+#Flights Class
 
 class flights:
-  def __init__(self, name, age):
-    self._name = name
-    self._age = age
+  def __init__(self, IATA_code, out_flight_no,  origin_airport, destination_airport, dept_date_time, flight_hours, stopover_dur, aircarft_type, fare_amount, in_flight_no = None):
+    self._IATA = IATA_code
+    self._outbound = out_flight_no
+    self._inbound = in_flight_no
+    self._origin = origin_airport
+    self._dest = destination_airport
+    self._dept_datetime = dept_date_time
+    self._flight_hours = flight_hours
+    self._stopover_dur = stopover_dur
+    self._fare_armount = fare_amount
+    self._aircraft_type = aircarft_type
+    
+  
+  def getIATA(self):
+    return self._IATA
+
+  def setIATA(self, IATA_code):
+    self._IATA = IATA_code
+    
+
+  def getOutbound(self): #Outbound Flight No.
+    return self._outbound
+
+  def setOutbound(self, out_flight_no):
+    self._outbound = out_flight_no
+
+  def getInbound(self): #Inbound Flight No.
+    return self._inbound
+
+  def setInbound(self, in_flight_no):
+    self._inbound = in_flight_no
+  
+  def getAircraftType(self):
+    return self._aircraft_type
+
+  def setAircraftType(self, aircraft_type):
+    self._aircraft_type = aircraft_type
+
+  def getOrigin(self):
+    return self._origin
+
+  def setOrigin(self, origin_airport):
+    self._origin = origin_airport
+  
+  def getDestination(self):
+    return self._dest
+
+  def setDestination(self, destination_airport):
+    self._dest = destination_airport
+  
+  def getDepartureDateTime(self):
+    return self._dept_datetime
+
+  def setDepartureDatetime(self, dept_date_time):
+    self._dept_datetime = dept_date_time
+  
+  def getFlightHours(self):
+    return self._flight_hours
+
+  def setFlightHours(self, flight_hours):
+    self._flight_hours = flight_hours
+  
+  def getStopoverDuration(self):
+    return self._stopover_dur
+
+  def setStepoverDuration(self, stopover_dur):
+    self._stopover_dur = stopover_dur
+  
+  def getFareAmount(self):
+    return self._fare_armount
+
+  def setFareAmount(self, fare_amount):
+    self._fare_armount = fare_amount
+  
+  
+
--- pyflights.py
+++ pyflights.py
@@ -1,8 +1,223 @@
 import datetime as dt
 import class_functions as cf
 
+
+
 def main():
-    pass
+    airlines_list = []
+    aircraft_type_list = []
+    scheduled_flights = []
+    
+    airlines_list, aircraft_type_list, scheduled_flights = cf.LoadInitialData(airlines_list, aircraft_type_list, scheduled_flights)
+    
+    while True:
+
+        user_input = input("Please choose from the following list of options:\n1: Create Airline\n2: Create Aircraft Type\n3: Create Flight\n4: Search and Book Flight\n")
+        
+
+        if user_input not in ["1", "2", "3", "4"]:
+            
+            print("\nERROR: You have entered an inalid input, Please try again\n")
+            continue
+
+        else:
+
+            if user_input == "1": #Create Airline
+                
+                user_input = input("\nPlease enter Airline Name followed by IATA Code, separated by commas as such: Singapore Airlines, SQ\n").strip().split(",")
+                
+                if len(user_input) != 2:
+                    print("\nERROR: You have entered an invalid amount of inputs, Please try again")
+               
+                else:
+
+                    airline_already_exist = False
+                    name = user_input[0].strip()
+                    IATA_code = user_input[1].strip()
+                    for items in airlines_list:
+                        if IATA_code.lower() == items.getIATA().lower():
+                            print("\nERROR: Airline already exists in the system\n")
+                            airline_already_exist = True
+                            break
+                    
+                    if airline_already_exist == False:
+                        cf.createAirline(name.capitalize(), IATA_code.upper(), airlines_list)
+                        print("\n Airline created successfully!\n")
+
+
+        
+            if user_input == "2": #Create Aircraft Type
+                
+                user_input = input("\nPlease input Airline IATA Code followed by Aircraft Type Name separated by commas as such: SQ, Boeing 747\n").strip().split(",")
+
+                if len(user_input) != 2:
+                    print("\nERROR: You have entered an invalid amount of inputs, Please try again")
+                
+                else:
+                    airlines_exist = False
+                    aircraft_type_exist = False
+
+                    IATA_code = user_input[0].strip()
+                    type_name = user_input[1].strip()
+
+                    for items in airlines_list:
+                        if IATA_code.lower() == items.getIATA().lower():
+                            airlines_exist = True
+                            break
+                    
+                    for items in aircraft_type_list:
+                        if type_name.lower() == items.getName().lower():
+                            aircraft_type_exist = True
+                            break
+                    
+                    if airlines_exist == True and aircraft_type_exist == False: #Only Case when Aircraft Type should be created
+                        
+                        cabin_class = []
+                        seat_config = {}
+                        seat_start_end = {}
+
+                        while True:
+
+                            user_input = input("Do you want to add Cabin Class? Y/N\n").lower()
+                            if user_input not in ["y", "n"]:
+                                print("\nERROR: You have entered an invalid input, Please try again\n")
+                                continue
+                            else:
+                                
+                                if user_input == "n":
+                                    
+                                    break
+                                
+                                else:
+                                    cabin_details = input("\nPlease enter Cabin Class details in this format:\nCabin Class , Seat Configuration, Starting Row Number, Ending Row Number(inclusive)\nY, 1-1-1 , 1 , 10\n").strip().split(",")
+                                    
+                                    if len(cabin_details) != 4:
+                                        print("\nERROR: You have entered an invalid amount of inputs, Please try again")
+                                        continue
+                                    else:
+                                        for i in range(0,len(cabin_details)):
+                                            cabin_details[i] = cabin_details[i].strip()
+                                        
+                                        if cabin_details[0].lower() not in ["y", "f", "j", "w"]:
+                                            print("\nERROR: You have entered an invalid Cabin Class, Please try again\n")
+                                            continue
+
+                                        if len(cabin_details[1].split("-")) <= 1 or len(cabin_details[1].split("-")) > 3:
+                                            print("\nERROR: You have entered an invalid Seat Configuration, Please try again\n")
+                                            continue
+                                        
+                                        if len(cabin_details[1].split("-")) == 3 or len(cabin_details[1].split("-")) == 2:
+                                            cabin_seat_split = cabin_details[1].split("-")
+                                            try:
+                                                for i in range(0,len(cabin_seat_split)):
+                                                    cabin_seat_split[i] = int(cabin_seat_split[i])
+                                            
+                                            except ValueError:
+                                                print("\nERROR: You have entered an invalid input for Seat Configuration, a numeric value split by - is expected\n")
+                                                continue
+
+                                        try:
+
+                                            cabin_details[2] = int(cabin_details[2])
+                                            cabin_details[3] = int(cabin_details[3])
+                                        
+                                        except ValueError:
+                                            print("\nERROR: You have entered an invalid input for either Starting Row Number OR Ending Row Number OR Both, a numeric value is expected\n")
+                                            continue
+
+                                        else:
+                                            if cabin_details[0].upper() not in cabin_class:
+                                                cabin_class.append(cabin_details[0].upper())
+                                                seat_config[cabin_details[0].upper()] = cabin_details[1]
+                                                seat_start_end[cabin_details[0].upper()] = [ cabin_details[2] , cabin_details[3]]
+                                                continue
+                                            else:
+                                                print("\nERROR: Cabin Class {} has already been added previously".format(cabin_details[0].upper()))
+                                                continue
+                                
+                        
+                        if len(cabin_class) == 0:
+                            
+                            print("\nERROR: Aircraft Type cannot be created without a Cabin Class, Please try again\n")
+                            
+                        else:
+                            
+                            aircraft_type_list.append(cf.aircraft_type(type_name.capitalize(), cabin_class, seat_config, seat_start_end))
+
+                            for items in airlines_list:
+                                if IATA_code.lower() == items.getIATA().lower():
+                                    items.setAircraftType(aircraft_type_list[-1])
+                                    break
+
+                            #Printing Output
+                            print("\nAircraft Type {} created for {}".format(type_name.capitalize(), IATA_code.upper()))
+                            
+                            no_of_seats_per_cabin = 0
+                            for items in cabin_class:
+                                no_of_seats_per_row_list = seat_config[items].strip().split("-")
+                                for i in range(0,len(no_of_seats_per_row_list)):
+                                    no_of_seats_per_row_list[i] = int(no_of_seats_per_row_list[i])
+                                no_of_seats_per_row = sum(no_of_seats_per_row_list)
+                                start_char = "A"
+                                end_char = chr(ord(start_char) + no_of_seats_per_row - 1)
+                                start_row = seat_start_end[items][0]
+                                end_row = seat_start_end[items][1]
+                                print("Cabin Class {} - Seat {}{} to {}{} through {}{} to {}{}, {} seats".format(items.upper(),start_row,start_char,start_row,end_char, end_row, start_char, end_row, end_char,  len(aircraft_type_list[-1].getSeatsLayout()[items])))
+                                no_of_seats_per_cabin = no_of_seats_per_cabin + len(aircraft_type_list[-1].getSeatsLayout()[items])
+                                                        
+                            print("Total {} seats".format(no_of_seats_per_cabin))
+
+                    elif airlines_exist == False and aircraft_type_exist == False:
+                        print("\nERROR: Given Airline does not exist in the system\n")
+
+                    elif airlines_exist == False and aircraft_type_exist == True:
+                        print("\nERROR: Given Airline does not exist in the system and Aircraft Type already exists in the system\n")
+
+                    elif airlines_exist == True and aircraft_type_exist == True:
+                        print("\nERROR: Aircraft Type already exists in the system\n")
+
+
+
+            if user_input == "3": #Create Flight
+                
+                user_input = input("\nPlease input following details separated by commas:\n1. Airline IATA Code\n2. Outbound Flight Number\n3. Return Flight Number(Optional)\n4. Origin Airport IATA Code\n5. Destination Airport IATA code\n6. Departure Date/Time eg. 5 Dec 22, 10:00 AM\n7. Flight Time eg. 6 hour 30 minute\n8. Stopover Duration eg. 3 hour\n9. Aircraft Type\n").strip().split(",")
+
+                if len(user_input) == 10 or len(user_input) == 9:
+                    
+
+                    #Get Fare Amount input once data validation are done
+                    user_input = input("\nPlease Input Fare Amount by Cabin Class separated by commas. Eg. Cabin Class F - $2000, Cabin Class J - $1000, Cabin Class Y - $500\n")
+
+
+                else:                       
+                    print("\nERROR: You have entered an invalid amount of inputs, Please try again")
+
+
+            if user_input == "4": #Search and Book Flight
+                pass
+
+
+        
+        while True:
+            
+            user_input = input("\nDo you want to proceed with another action? Y/N\n").lower()
+            
+            if user_input in ["y", "n"]:
+
+                break
+
+            else:
+
+                print("\nERROR: You have entered an invalid input, Please try again\n")
+                continue
+
+        if user_input == "y":
+            
+            continue
+
+        elif user_input == "n":
+            
+            break
 
 
 
